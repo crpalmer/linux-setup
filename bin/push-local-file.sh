d70417fd27eb74246b1467ce66ddd41e653c228d
Christopher R. Palmer <crpalmer@gmail.com>
2015-07-08 06:12:30 -0400

    push-local-file: Use a temporary folder on /sdcard/

--- bin/push-local-file.sh
+++ bin/push-local-file.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -e
 
+TMP=/sdcard/push
+
 dir="$1"
 shift
 full_file="$1"
@@ -13,10 +15,11 @@ fi
 
 file=`basename $full_file`
 echo "Pushing: $dir/$file"
-adb push "$full_file" /sdcard/$file
+adb shell mkdir -p $TMP
+adb push "$full_file" $TMP/$file
 adb shell su -c "mount -o remount,rw /system"
 adb shell su -c "mv $dir/$file $dir/$file.old"
-adb shell su -c "cp '/sdcard/$file' '$dir/'"
+adb shell su -c "cp '$TMP/$file' '$dir/'"
 adb shell su -c "chmod $perms $dir/$file"
 adb shell su -c "restorecon -v $dir/$file"
 adb shell su -c "mount -o remount,ro /system"
